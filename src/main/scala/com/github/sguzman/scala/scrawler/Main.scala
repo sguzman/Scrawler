b52d61955997de64b232a60cfa12aad80ffc392a
Salvador Guzman <guzmansalv@gmail.com>
2017-11-25 13:54:36 -0800

    grab cookie from cmd args

--- src/main/scala/com/github/sguzman/scala/scrawler/Main.scala
+++ src/main/scala/com/github/sguzman/scala/scrawler/Main.scala
@@ -1,7 +1,9 @@
 package com.github.sguzman.scala.scrawler
 
+import com.github.sguzman.scala.scrawler.jcommander.Args
+
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+    val argv = Args(args)
   }
 }
