64b157a841c576793ec68b1c7443ce68bd68a970
Salvador Guzman <guzmansalv@gmail.com>
2017-11-26 06:30:42 -0800

    create type for return json

---
+++ src/main/scala/com/github/sguzman/scala/scrawler/jsontypecheck/return/Trips.scala
@@ -0,0 +1,7 @@
+package com.github.sguzman.scala.scrawler.jsontypecheck.`return`
+
+import com.github.sguzman.scala.scrawler.jsontypecheck.trip.Trip
+
+case class Trips(
+                trips: Array[Trip]
+                )
