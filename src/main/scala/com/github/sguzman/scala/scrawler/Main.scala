98573b7ce666912ef8a392941015535b1055367e
Salvador Guzman <guzmansalv@gmail.com>
2017-11-26 04:08:17 -0800

    added trip getter to main

--- src/main/scala/com/github/sguzman/scala/scrawler/Main.scala
+++ src/main/scala/com/github/sguzman/scala/scrawler/Main.scala
@@ -1,8 +1,11 @@
 package com.github.sguzman.scala.scrawler
 
+import java.util.UUID
+
 import com.github.sguzman.scala.scrawler.jcommander.Args
 import com.github.sguzman.scala.scrawler.jsontypecheck.StatementListObject
 import com.github.sguzman.scala.scrawler.jsontypecheck.statement.Statement
+import com.github.sguzman.scala.scrawler.jsontypecheck.trip.Trip
 import com.google.gson.GsonBuilder
 import org.feijoas.mango.common.base.Preconditions
 
@@ -20,15 +23,35 @@ object Main {
 
     val stmts = getStatement(argv, _: StatementListObject)
     val statements = jsonTypes.par.map(stmts).filter(_.isDefined).map(_.get)
-    val tripUuids = statements.map(_.body.driver.trip_earnings.trips.keySet).flatMap(_.toArray)
+    val tripUuids = statements.map(_.body.driver.trip_earnings.trips.keySet).flatMap(_.toArray).map(_.asInstanceOf[UUID])
 
-    println(tripUuids)
+    val trp = getTrip(argv, _: UUID)
+    val trips = tripUuids.par.map(trp).filter(_.isDefined).map(_.get)
+
+    println(trips)
   }
 
-  def getStatement(argv: Args, statement: StatementListObject) = {
+  def getTrip(argv: Args, uuid: UUID): Option[Trip] = {
+    try {
+      val uuidString = uuid.toString
+      println(s"Trip uuid $uuid")
+      val url = s"https://partners.uber.com/p3/money/trips/trip_data/$uuidString"
+
+      val request = Http(url).header("Cookie", argv.cookie)
+      val response = request.asString
+
+      val gson = new GsonBuilder().create
+      val jsonType = gson.fromJson(response.body, classOf[Trip])
+      Some(jsonType)
+    } catch {
+      case _: Throwable => None
+    }
+  }
+
+  def getStatement(argv: Args, statement: StatementListObject): Option[Statement] = {
     try {
       val uuid = statement.uuid.toString
-      println(uuid)
+      println(s"Statement $uuid")
       val url = s"https://partners.uber.com/p3/money/statements/view/$uuid"
       val request = Http(url).header("Cookie", argv.cookie)
       val response = request.asString
