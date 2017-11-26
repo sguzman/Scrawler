2d53fde890c9196e23e78712384d02b34ffe3340
Salvador Guzman <guzmansalv@gmail.com>
2017-11-26 01:44:15 -0800

    simple application of gson

--- src/main/scala/com/github/sguzman/scala/scrawler/Main.scala
+++ src/main/scala/com/github/sguzman/scala/scrawler/Main.scala
@@ -2,8 +2,8 @@ package com.github.sguzman.scala.scrawler
 
 import com.github.sguzman.scala.scrawler.jcommander.Args
 import com.github.sguzman.scala.scrawler.jsontypecheck.StatementListObject
-import io.circe.generic.auto._
-import io.circe.parser._
+import com.github.sguzman.scala.scrawler.jsontypecheck.statement.Statement
+import com.google.gson.GsonBuilder
 import org.feijoas.mango.common.base.Preconditions
 
 import scalaj.http.{Http, HttpResponse}
@@ -17,7 +17,29 @@ object Main {
     Preconditions.checkArgument(success == 200, "Cookie was not valid")
 
     val jsonTypes = getStatementList(argv)
-    println(jsonTypes)
+
+    val stmts = getStatement(argv, _: StatementListObject)
+    val statements = jsonTypes.par.map(stmts).filter(_.isDefined).map(_.get)
+    val tripUuids = statements.map(_.body.driver.trip_earnings.trips.keySet).flatMap(_.toArray)
+
+    println(tripUuids)
+  }
+
+  def getStatement(argv: Args, statement: StatementListObject) = {
+    try {
+      val uuid = statement.uuid.toString
+      println(uuid)
+      val url = s"https://partners.uber.com/p3/money/statements/view/$uuid"
+      val request = Http(url).header("Cookie", argv.cookie)
+      val response = request.asString
+      val gson = new GsonBuilder().create
+
+      val jsonType = gson.fromJson(response.body, classOf[Statement])
+
+      Some(jsonType)
+    } catch {
+      case _: Throwable => None
+    }
   }
 
   def getStatus(argv: Args): HttpResponse[String] = {
@@ -29,11 +51,11 @@ object Main {
   }
 
   def getStatementList(argv: Args): Array[StatementListObject] = {
-    val url2 = "https://partners.uber.com/p3/money/statements/all_data/"
-    val request2 = Http(url2).header("Cookie", argv.cookie)
-    val response2 = request2.asString
-
-    val jsonTypes = decode[Array[StatementListObject]](response2.body)
+    val url = "https://partners.uber.com/p3/money/statements/all_data/"
+    val request = Http(url).header("Cookie", argv.cookie)
+    val response = request.asString
+    val gson = new GsonBuilder().create()
+    val jsonTypes = gson.fromJson(response.body, classOf[Array[StatementListObject]])
 
     jsonTypes
   }
